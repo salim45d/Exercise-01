cd79a059f5c8a07f046eee3a0fb38ed6b26a9974
salim45d <salim.agus@live.com>
2024-08-30 20:56:27 +0800

    feat: Rectangle Area and Perimeter Code

---
+++ a-rectangle.ts
@@ -0,0 +1,25 @@
+//////////////
+//RECTANGLE//
+////////////
+
+// Declare berapa panjang dan lebar dari Persegi Panjang disimpan dalam variable
+const lengthRectangle : number = 5;  
+const witdhRectangle : number= 3;
+
+
+    ///////////////////
+    //RECTANGLE AREA//
+    /////////////////
+    const areaRectangle: number = lengthRectangle * witdhRectangle;
+
+    //Output
+    console.log(areaRectangle);
+
+
+    ////////////////////////
+    //RECTANGLE PERIMETER//
+    //////////////////////
+    const perimeterRectangle: number = 2 * (lengthRectangle + witdhRectangle);
+
+    //Output
+    console.log(perimeterRectangle);
